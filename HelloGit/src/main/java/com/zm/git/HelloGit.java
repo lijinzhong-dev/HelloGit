2ddbb0b4d1e55e0adf76a91210073b9026db28b9
lijinzhong-dev <535677006@qq.com>
2020-07-13 23:15:48 +0800

    第一次提交文件到本地库

---
+++ HelloGit/src/main/java/com/zm/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.zm.git;
+
+/**
+ * @Auther: lijinzhong
+ * @Date: 2020/7/13
+ * @Description: com.zm.git
+ * @version: 1.0
+ */
+public class HelloGit {
+     public static void main(String[] args) {
+       System.out.println("Hello Git");
+      }
+}
