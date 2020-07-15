a3896dbf4df073b77136263bd10314c73fae74a9
lijinzhong-dev <535677006@qq.com>
2020-07-15 11:12:02 +0800

    分支添加

--- HelloGit/src/main/java/com/zm/git/HelloGit.java
+++ HelloGit/src/main/java/com/zm/git/HelloGit.java
@@ -8,7 +8,7 @@ package com.zm.git;
  */
 public class HelloGit {
      public static void main(String[] args) {
-       System.out.println("Hello Git");
+       System.out.println("Hello Git 分支添加");
        System.out.println("更新1");
        System.out.println("创建分支");
       }
